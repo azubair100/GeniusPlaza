af71806979da5ef028532807fb9d843ba2cd303f
Ahmed Zubair <iambatman8102@gmail.com>
2019-10-24 20:40:59 -0400

    views- mainactivity and listfragments are chained correctly

--- app/src/main/java/com/zubair/geniusplaza/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/zubair/geniusplaza/viewmodels/ListViewModel.kt
@@ -1,6 +1,32 @@
 package com.zubair.geniusplaza.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.zubair.geniusplaza.datasources.UsersDataSourceFactory
+import com.zubair.geniusplaza.models.User
+import com.zubair.geniusplaza.network.UserService
+import io.reactivex.disposables.CompositeDisposable
 
 class ListViewModel : ViewModel(){
+    var users: LiveData<PagedList<User>>
+    private val disposable = CompositeDisposable()
+    private val userService: UserService = UserService()
+    private val sourceFactory: UsersDataSourceFactory
+
+    init {
+        sourceFactory = UsersDataSourceFactory(disposable, userService)
+        val config = PagedList.Config.Builder()
+            .setPageSize(6)
+            .setInitialLoadSizeHint(6)
+            .setEnablePlaceholders(false)
+            .build()
+        users = LivePagedListBuilder<Int, User>(sourceFactory, config).build()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
 }
--- app/src/main/java/com/zubair/geniusplaza/views/ListFragment.kt
+++ app/src/main/java/com/zubair/geniusplaza/views/ListFragment.kt
@@ -6,21 +6,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.zubair.geniusplaza.R
+import com.zubair.geniusplaza.adapters.UserAdapter
+import com.zubair.geniusplaza.models.User
+import com.zubair.geniusplaza.viewmodels.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class ListFragment : Fragment() {
+
+    private lateinit var listViewModel: ListViewModel
+    private lateinit var userAdapter: UserAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        listViewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        retainInstance = true
+
+        userAdapter = UserAdapter ()
+        userAdapter.setHasStableIds(true)
+
+        listViewModel.users.observe(this, Observer<PagedList<User>> {
+            userAdapter.submitList(it)
+            if (userAdapter.currentList!!.size > 0) {
+
+            }
+        })
+
+    }
+
+    private fun goToUserCreationFragment() {
+        activity!!.supportFragmentManager.beginTransaction()
+            .replace(R.id.main_activity_container, NewUserFragment())
+            .addToBackStack("new_user_fragment").commit()
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        create_new_user.setOnClickListener(View.OnClickListener { goToUserCreationFragment()})
+
+    }
+    private fun initAdapter() {
+        val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        user_list_rv.layoutManager = linearLayoutManager
+        user_list_rv.adapter = userAdapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_list, container, false)
+
 
 
 }
