827f25a2fe431cf4effb8bd863bf0bc30ebd2c99
Ahmed Zubair <iambatman8102@gmail.com>
2019-10-24 20:47:03 -0400

    views - updated the new user pag

--- app/src/main/java/com/zubair/geniusplaza/viewmodels/NewUserViewModel.kt
+++ app/src/main/java/com/zubair/geniusplaza/viewmodels/NewUserViewModel.kt
@@ -1,6 +1,37 @@
 package com.zubair.geniusplaza.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.zubair.geniusplaza.models.NewUser
+import com.zubair.geniusplaza.network.NewUserResponse
+import com.zubair.geniusplaza.network.UserService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class NewUserViewModel: ViewModel() {
+    val newUserCreated = MutableLiveData<Boolean>()
+    private val disposable = CompositeDisposable()
+    private val userService: UserService = UserService()
+
+    fun createNewUser(user : NewUser){
+        disposable.add(
+            userService.createUser(user)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<NewUserResponse>() {
+                    override fun onSuccess(response: NewUserResponse) {
+                        if(response.email == user.email) newUserCreated.value = true
+                    }
+                    override fun onError(e: Throwable) { newUserCreated.value = false}
+                }
+                )
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
--- app/src/main/java/com/zubair/geniusplaza/views/NewUserFragment.kt
+++ app/src/main/java/com/zubair/geniusplaza/views/NewUserFragment.kt
@@ -6,21 +6,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 import com.zubair.geniusplaza.R
+import com.zubair.geniusplaza.models.NewUser
+import com.zubair.geniusplaza.viewmodels.NewUserViewModel
+import kotlinx.android.synthetic.main.fragment_new_user.*
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class NewUserFragment : Fragment() {
+    private lateinit var newUserViewModel: NewUserViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        newUserViewModel = ViewModelProviders.of(this).get(NewUserViewModel::class.java)
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        newUserViewModel.newUserCreated.observe(this, Observer { user ->
+            if(user){
+                Toast.makeText(context, getString(R.string.success), Toast.LENGTH_LONG).show()
+                goToUserListFragment()
+            }
+            else{
+                Toast.makeText(context, getString(R.string.fail), Toast.LENGTH_LONG).show()
+            }
+            progress_bar.visibility = View.INVISIBLE
+            first_name.visibility = View.VISIBLE
+            last_name.visibility = View.VISIBLE
+            email.visibility = View.VISIBLE
+        })
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_user, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_new_user, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        newUserButton.setOnClickListener(View.OnClickListener {
+            progress_bar.visibility = View.VISIBLE
+            first_name.visibility = View.INVISIBLE
+            last_name.visibility = View.INVISIBLE
+            email.visibility = View.INVISIBLE
+
+            newUserViewModel.createNewUser(
+                NewUser(
+                first_name.text.toString(),
+                last_name.text.toString(),
+                email.text.toString()
+            )
+            )
+        })
     }
 
+    private fun goToUserListFragment() {
+        activity!!.supportFragmentManager.popBackStack()
+        activity!!.supportFragmentManager.beginTransaction()
+            .replace(R.id.main_activity_container, ListFragment())
+            .addToBackStack(null).commit()
+    }
 
 }
