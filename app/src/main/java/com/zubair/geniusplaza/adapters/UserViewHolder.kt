e2b740306d0f7b9ce26d9d2bbcbf469aec94f719
Ahmed Zubair <iambatman8102@gmail.com>
2019-10-24 20:30:30 -0400

    created related layouts, data source files for paging and recyclerview components

---
+++ app/src/main/java/com/zubair/geniusplaza/adapters/UserViewHolder.kt
@@ -0,0 +1,27 @@
+package com.zubair.geniusplaza.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.zubair.geniusplaza.R
+import com.zubair.geniusplaza.models.User
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bindTo(user: User?) {
+        itemView.UserName.text = user?.firstName + " " + user?.lastName
+        Picasso.get().load(user?.url).into(itemView.UserAvatar)
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): UserViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.item_user, parent, false)
+            return UserViewHolder(view)
+        }
+    }
+
+}
